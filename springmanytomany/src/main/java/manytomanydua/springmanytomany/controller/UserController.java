5107d3882c3606d1dec27bb61d4c8dba3cfdab44
CindoddCindy <cindodcindy@gmail.com>
2020-12-30 15:26:45 +0700

    copy class

---
+++ springmanytomany/src/main/java/manytomanydua/springmanytomany/controller/UserController.java
@@ -0,0 +1,44 @@
+package manytomanydua.springmanytomany.controller;
+
+import manytomanydua.springmanytomany.entitiy.User;
+import manytomanydua.springmanytomany.model.UserModel;
+import manytomanydua.springmanytomany.repository.UserRepository;
+import manytomanydua.springmanytomany.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private UserService userService;
+    private UserRepository userRepository;
+
+    public UserController(UserService userService, UserRepository userRepository) {
+        this.userService = userService;
+        this.userRepository = userRepository;
+    }
+    @PostMapping("/user/create")
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+    @GetMapping("/user/details/{id}")
+    public UserModel getUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+    @GetMapping("/user/all")
+    public List<UserModel> getUsers() {
+        return userService.getUsers();
+
+    }
+    @PutMapping("/user/update/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(user, id);
+    }
+    @DeleteMapping("user/delete/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable Long id) {
+        return userService.deleteUser(id);
+    }
+
+}
---
+++ springmanytomany/src/main/java/manytomanydua/springmanytomany/model/RoleModel.java
@@ -0,0 +1,18 @@
+package manytomanydua.springmanytomany.model;
+
+public class RoleModel {
+    private String name;
+    private String description;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
