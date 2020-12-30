ee05a6e9ef8613c17786ce62e7edae7dcc19425e
CindoddCindy <cindodcindy@gmail.com>
2020-12-30 14:35:45 +0700

    tambah folder

---
+++ springmanytomany/src/main/java/manytomanydua/springmanytomany/SpringmanytomanyApplication.java
@@ -0,0 +1,13 @@
+package manytomanydua.springmanytomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmanytomanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmanytomanyApplication.class, args);
+	}
+
+}
